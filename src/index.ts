03895749e4db4e75155fd546c6ce4e44ba215fde
khaledebdelli <khaled88ebdelli@gmail.com>
2022-03-03 08:54:36 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+app.get("/test", (req, res) => {
+  res.status(200).send("Hello World");
+});
+app.listen(PORT, () => {
+  console.log(`Server running on  http://localhost:${PORT}`);
+});
