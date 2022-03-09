cd19fd34070c37294c75b52ca4ee299aaa27430a
khaledebdelli <khaled88ebdelli@gmail.com>
2022-03-09 19:52:17 +0100

    install dotenv and fix endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname+'/.env' });
+
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
-app.get("/check/health", (req, res) => {
-  res.status(200).send("Hello World");
+app.get("/api/env", (req, res) => {
+  res.status(200).send("environment.*${process.env.NODE_ENV}");
 });
-app.get("/check/version", (req, res) => {
-  res.status(200).send(`"Hello World!.*${process.env.CIRCLE_SHA1}_${process.env.CIRCLE_BUILD_NUM}"`);
+app.get("/api/version", (req, res) => {
+  res.status(200).send(`CIRCLE_SHA1=${process.env.CIRCLE_SHA1}_CIRCLE_BUILD_NUM=${process.env.CIRCLE_BUILD_NUM}`);
 });
 app.listen(PORT, HOST, () => {
   console.log(`Server running on  ${HOST}:${PORT}`);
