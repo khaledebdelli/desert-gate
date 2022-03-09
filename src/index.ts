670c929a1a06e0578617522cfa2749fec8366adf
khaledebdelli <khaled88ebdelli@gmail.com>
2022-03-09 12:04:29 +0100

    fix endpoints check for api

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import express from "express";
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const app = express();
-app.get("/check", (req, res) => {
+app.get("/check/health", (req, res) => {
   res.status(200).send("Hello World");
+});
+app.get("/check/version", (req, res) => {
+  res.status(200).send(`"Hello World!.*${process.env.CIRCLE_SHA1}_${process.env.CIRCLE_BUILD_NUM}"`);
 });
 app.listen(PORT, HOST, () => {
   console.log(`Server running on  ${HOST}:${PORT}`);
