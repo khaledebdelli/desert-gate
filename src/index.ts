11b0bde4b86e121c74953e0871ef68c120273521
khaledebdelli <khaled88ebdelli@gmail.com>
2022-03-09 20:04:17 +0100

    fix env vars in crcleci

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import * as dotenv from "dotenv";
 
-dotenv.config({ path: __dirname+'/.env' });
+dotenv.config();
 
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = Number(process.env.PORT) || 8080;
+const HOST = process.env.HOST || 'localhost';
+
 const app = express();
-app.get("/api/env", (req, res) => {
-  res.status(200).send("environment.*${process.env.NODE_ENV}");
+app.get("/api/environment", (req, res) => {
+  res.status(200).send(`environment.${process.env.NODE_ENV}`);
 });
 app.get("/api/version", (req, res) => {
   res.status(200).send(`CIRCLE_SHA1=${process.env.CIRCLE_SHA1}_CIRCLE_BUILD_NUM=${process.env.CIRCLE_BUILD_NUM}`);
